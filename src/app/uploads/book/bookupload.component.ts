21c83b99bcb8d78d4be5e438e9a29c7344053883
sid12261996 <sidharthrkc@gmail.com>
2019-06-13 17:17:30 +0530

    validation for the file input at bulk books upload

--- src/app/uploads/book/bookupload.component.ts
+++ src/app/uploads/book/bookupload.component.ts
@@ -1,24 +1,39 @@
 import {Component, OnInit} from '@angular/core';
+import {ValidationService} from '../../_shared/validation.service';
 
 @Component({
   selector: 'app-bookupload',
   templateUrl: './bookupload.component.html',
   styleUrls: ['./bookupload.component.css']
 })
-export class BookUploadComponent  {
-  constructor() {
+export class BookUploadComponent {
+  fileError = true;
+  validationErrors: string[];
+  fileUploadQueue: any;
+
+  constructor(private _validationService: ValidationService) {
   }
 
   onSubmit() {
-console.log('On submit');
+    console.log('On submit');
   }
 
   onDownload() {
     console.log('On Download');
   }
 
-  upload($event: MouseEvent) {
-    console.log('On upload', $event);
+
+  upload($event: any) {
+
+    const validate = this._validationService.fileValidation($event.target.files[0]);
+
+    if (!validate.isValid) {
+      this.fileError = true;
+      this.validationErrors = validate.Errors;
+    } else {
+      this.fileError = false;
+    }
+    console.log('valid Status:', validate);
   }
 }
 
